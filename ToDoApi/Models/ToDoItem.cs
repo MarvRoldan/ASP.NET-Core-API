a6a7c49c58cff894d19c8663f57e12fec09dedc5
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-21 11:44:39 -0600

    Added models

---
+++ ToDoApi/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public int Task { get; set; }
+        public int Completed { get; set; }
+    }
+}
